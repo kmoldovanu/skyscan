a4d8aad87b515a3b7811994051a9c7a7fc526137
konstantinos <k.moldovanu@datafood.it>
2026-01-27 17:05:42 +0100

    added bs5 and tailwind, [WIP]

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/booking">Booking</Link> | <Link to="/my-trips">My Trips</Link>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <NavLink to="/" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}>Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/booking" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}>Booking</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/my-trips" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}>My Trips</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
